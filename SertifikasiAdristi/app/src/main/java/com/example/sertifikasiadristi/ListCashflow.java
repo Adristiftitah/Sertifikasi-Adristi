909c19e99a4bd28210e61d55968517b51dfa269a
Adristiftitah <adristiiftitahyuniar@gmail.com>
2022-09-17 19:28:42 +0700

    Sertifikasi DIPA 2022

---
+++ SertifikasiAdristi/app/src/main/java/com/example/sertifikasiadristi/ListCashflow.java
@@ -0,0 +1,13 @@
+package com.example.sertifikasiadristi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListCashflow extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_cashflow);
+    }
+}
